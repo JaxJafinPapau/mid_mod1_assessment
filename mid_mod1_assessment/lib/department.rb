d72934428e44b5b539c52d6bad2bb9ca9c0bd658
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-10 11:22:10 -0700

    Initial commit

---
+++ mid_mod1_assessment/lib/department.rb
@@ -0,0 +1,7 @@
+require './lib/employee'
+
+class Department
+  def initialize(argument)
+    @argument = argument
+  end
+end
---
+++ mid_mod1_assessment/test/department_test.rb
@@ -0,0 +1,10 @@
+require './lib/employee'
+require './lib/department'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class DepartmentTest < Minitest::Test
+  def method_name
+
+  end
+end
