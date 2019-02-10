35c7fe472db0786015aee143a6bf785a9c12534f
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-10 11:24:13 -0700

    Adds setup method to test

--- mid_mod1_assessment/test/department_test.rb
+++ mid_mod1_assessment/test/department_test.rb
@@ -4,7 +4,8 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class DepartmentTest < Minitest::Test
-  def method_name
-
-  end
+  def setup
+    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    @aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+    @customer_service = Department.new("Customer Service")
 end
